6ab96175e02fc4b20ed12e4fbdb18eac35e095c9
suxxus <suxxus@outlook.com>
2016-07-08 13:59:52 +0200

    variadic-and-compose

---
+++ src/recipes-with-basic-functions/variadic-compose-and-recursion.js
@@ -0,0 +1,34 @@
+(function() {
+
+    var compose = function() {
+        var slice = [].slice,
+            fns = slice.call(arguments);
+
+        return function(val) {
+            return fns.reverse()
+                .reduce(
+                    function(acc, fn) {
+                        return fn(acc);
+                    }, val
+                );
+        }
+    }
+
+    var addOne = function(a) {
+        return a + 1;
+    };
+
+    var addTwo = function(a) {
+        return a + 2;
+    }
+
+    var logResult = function(res) {
+        console.log('your value is: ', res);
+    }
+
+    const yourResult = compose(logResult, addTwo, addOne);
+
+    yourResult(1);
+
+
+}());
