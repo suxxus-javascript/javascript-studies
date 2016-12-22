3d8c4d62b010831f96be26abb44d98a915b93c60
suxxus <suxxus@outlook.com>
2016-12-22 11:19:44 +0100

    lesson 2

---
+++ src/composable-functions/02-refactor-imperative-code-to-a-single-composed-expression.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var box = function(x) {
+
+    return {
+
+        map: function(fn) {
+
+            return box(fn(x));
+
+        },
+
+        fold: function(fn) {
+
+            return fn(x);
+        },
+
+        inspect: function() {
+
+            return 'box(' + x + ')';
+
+        }
+
+    };
+
+};
+
+// var moneyToFloat = function moneyToFloat(str) {
+
+//     return parseFloat(str.replace(/$/g, ''));
+
+// };
+
+var moneyToFloat = function(str) {
+
+    return box(str.replace(/$/g, ''))
+        .map(function(replaced) {
+
+            return parseFloat(replaced);
+
+        });
+
+};
+
+// var percentToFloat = function percentToFloat(str) {
+
+//     return parseFloat(
+//         str.replace(/%/g, '')
+//     ) * 0.01;
+
+// };
+
+var percentToFloat = function(str) {
+
+    return box(str.replace(/%/g))
+        .map(function(replaced) {
+
+            return parseFloat(replaced);
+
+        })
+        .map(function(num) {
+
+            return num * 0.01;
+
+        });
+
+};
+
+// var applyDiscount = function(price, discount) {
+
+//     var cost = moneyToFloat(price);
+//     var savings = percentToFloat(discount);
+//     return cost - (cost * savings);
+
+// };
+
+var applyDiscount = function(price, discount) {
+
+    return moneyToFloat(price)
+        .fold(function(cost) {
+
+            return percentToFloat(discount)
+                .fold(function(savings) {
+
+                    return cost - (cost * savings);
+
+                });
+
+        });
+
+};
+
+console.log('moneyToFloat--> ', moneyToFloat('1.24$'));
+console.log('percentToFloat--> ', percentToFloat('20%'));
+console.log('applyDiscount--> ', applyDiscount('1.24$', '10%'));
