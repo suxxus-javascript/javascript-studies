755aedf18ad9e1b81d5b6dc8c6a3b3f9ac46c8f0
suxxus <suxxus@outlook.com>
2017-01-27 19:38:40 +0100

    renamed function fp ramda discount

--- src/fp-ramda/01-price-discount-composing.js
+++ src/fp-ramda/01-price-discount-composing.js
@@ -2,12 +2,12 @@ const R = require('ramda');
 const percentToFloat = R.compose(R.multiply(0.01), R.replace(/%/g, ''));
 const removeCurrencySimbol = R.compose(Number, R.replace(/\$|€/g, ''));
 const calcFinalPrice = R.curry((cost, savings) => cost - (cost * savings));
-const getFinalPrice = (price, disc) =>
+const applyDiscount = (price, disc) =>
     R.compose(
         (fn) => fn(percentToFloat(disc)),
         (fn) => fn(removeCurrencySimbol(price))
     )(calcFinalPrice);
 
 console.log(
-    getFinalPrice('12$', '10%')
+    applyDiscount('12$', '10%')
 );
