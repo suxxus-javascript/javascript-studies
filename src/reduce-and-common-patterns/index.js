793a1842ff6c903bb4f71cc8c46e79e53357f182
suxxus <suxxus@outlook.com>
2017-08-04 11:58:42 +0200

    Added compose pattern implemented with reduce

--- src/reduce-and-common-patterns/index.js
+++ src/reduce-and-common-patterns/index.js
@@ -2,41 +2,66 @@
 
 (function() {
 
-    // https://egghead.io/lessons/javascript-introducing-reduce-common-patterns
+  // https://egghead.io/lessons/javascript-introducing-reduce-common-patterns
 
-    var data = [1, 2, 3, 4];
+  var data = [1, 2, 3, 4];
 
-    var doubled = data.reduce(function(acc, value) {
-        acc.push(value * 2);
-        return acc;
+  var doubled = data.reduce(function(acc, value) {
+    acc.push(value * 2);
+    return acc;
+  }, []);
+  console.log('my doubled data', doubled);
+
+  var doubledMapped = data.map(function(item) {
+    return item * 2;
+  }, []);
+  console.log('my doubled mapped', doubledMapped);
+
+  var data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  var evens = data2.reduce(function(acc, value) {
+    if ((value % 2) === 0) {
+      acc.push(value);
+    }
+    return acc;
+  }, []);
+  console.log('evens ', evens);
+
+  var evensFiltered = data2.filter(function(item) {
+    return (item % 2) === 0;
+  });
+  console.log('evens filtered ', evensFiltered);
+
+  var filteredMapped = data2.filter(function(item) {
+    return (item % 2) === 0;
+  }).map(function(item) {
+    return item * 2;
+  });
+  console.log('filtered mapped: ', filteredMapped);
+
+  var multiplyByTwo = function(x) { return x * 2 };
+
+  var doubledMappedWithReduce = function(fn, xs) {
+    return xs.reduce(function(acc, value) {
+      return acc.concat(fn(value));
     }, []);
-    console.log('my doubled data', doubled);
+  }
 
-    var doubledMapped = data.map(function(item) {
-        return item * 2;
-    }, []);
-    console.log('my doubled mapped', doubledMapped);
+  console.log('double mapped with reduce', doubledMappedWithReduce(multiplyByTwo, data));
 
-    var data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 
-    var evens = data2.reduce(function(acc, value) {
-        if ((value % 2) === 0) {
-            acc.push(value);
-        }
-        return acc;
-    }, []);
-    console.log('evens ', evens);
+  var compose = function() {
+    var fns = [].slice.call(arguments);
+    return function(x) {
+      return fns.reduceRight(function(v, f) {
+        return f(v)
+      }, x);
+    }
+  };
 
-    var evensFiltered = data2.filter(function(item) {
-        return (item % 2) === 0;
-    });
-    console.log('evens filtered ', evensFiltered);
-
-    var filteredMapped = data2.filter(function(item) {
-        return (item % 2) === 0;
-    }).map(function(item) {
-        return item * 2;
-    });
-    console.log('filtered mapped: ', filteredMapped);
+  console.log(
+    compose(function(x) {return x * 2 }, function(x) {return x + 2 })(5)
+  );
 
 }());
